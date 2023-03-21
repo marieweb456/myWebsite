9352950cfbd241da2750d8dd3b30339f0a78a7bc
github_ayns789 <joly.a.idf@gmail.com>
2023-03-21 17:20:49 +0100

    import dotenv and create toml

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,58 @@
+// const path = require('path');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// const isProduction = process.env.NODE_ENV === 'production';
+
+// const stylesHandler = MiniCssExtractPlugin.loader;
+
+// const config = {
+//   entry: './src/index.js',
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: 'main.js',
+//   },
+//   plugins: [
+//     new MiniCssExtractPlugin(),
+//     new HtmlWebpackPlugin({
+//       template: './index.html',
+//       filename: 'index.html',
+//     }),
+//   ],
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(js|jsx)$/i,
+//         loader: 'babel-loader',
+//       },
+//       {
+//         test: /\.css$/i,
+//         use: [stylesHandler, 'css-loader'],
+//       },
+//       {
+//         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+//         type: 'asset',
+//       },
+//     ],
+//   },
+//   resolve: {
+//     fallback: {
+//       fs: false,
+//       path: require.resolve('path-browserify'),
+//       os: require.resolve('os-browserify/browser'),
+//       //   crypto: require.resolve('crypto-browserify'),
+//     },
+//   },
+// };
+
+// module.exports = () => {
+//   if (isProduction) {
+//     config.mode = 'production';
+//   } else {
+//     config.mode = 'development';
+//   }
+//   return config;
+// };
 const path = require('path');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -43,6 +98,9 @@ const config = {
       //   crypto: require.resolve('crypto-browserify'),
     },
   },
+  node: {
+    fs: 'empty',
+  },
 };
 
 module.exports = () => {
@@ -51,6 +109,7 @@ module.exports = () => {
   } else {
     config.mode = 'development';
   }
+
   return config;
 };
 
