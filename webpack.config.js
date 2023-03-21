fbbb460e478f14da3e68b96a1301f5234610bc22
github_ayns789 <joly.a.idf@gmail.com>
2023-03-21 15:30:49 +0100

    clean cache npm and install package.json

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,58 @@
-const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+// const path = require('path');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
 
-const isProduction = process.env.NODE_ENV === 'production';
+// const isProduction = process.env.NODE_ENV === 'production';
 
-const stylesHandler = MiniCssExtractPlugin.loader;
+// const stylesHandler = MiniCssExtractPlugin.loader;
 
-const config = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-  },
-  plugins: [
-    new MiniCssExtractPlugin(),
-    new HtmlWebpackPlugin({
-      template: './public/index.html',
-      filename: 'index.html',
-    }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/i,
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.css$/i,
-        use: [stylesHandler, 'css-loader'],
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: 'asset',
-      },
-    ],
-  },
-  resolve: {
-    fallback: {
-      fs: false,
-      path: require.resolve('path-browserify'),
-      os: require.resolve('os-browserify/browser'),
-      crypto: require.resolve('crypto-browserify'),
-    },
-  },
-};
+// const config = {
+//   entry: './src/index.js',
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: 'main.js',
+//   },
+//   plugins: [
+//     new MiniCssExtractPlugin(),
+//     new HtmlWebpackPlugin({
+//       template: './public/index.html',
+//       filename: 'index.html',
+//     }),
+//   ],
+//   module: {
+//     rules: [
+//       {
+//         test: /\.(js|jsx)$/i,
+//         loader: 'babel-loader',
+//       },
+//       {
+//         test: /\.css$/i,
+//         use: [stylesHandler, 'css-loader'],
+//       },
+//       {
+//         test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+//         type: 'asset',
+//       },
+//     ],
+//   },
+//   resolve: {
+//     fallback: {
+//       fs: false,
+//       path: require.resolve('path-browserify'),
+//       os: require.resolve('os-browserify/browser'),
+//       crypto: require.resolve('crypto-browserify'),
+//     },
+//   },
+// };
 
-module.exports = () => {
-  if (isProduction) {
-    config.mode = 'production';
-  } else {
-    config.mode = 'development';
-  }
-  return config;
-};
+// module.exports = () => {
+//   if (isProduction) {
+//     config.mode = 'production';
+//   } else {
+//     config.mode = 'development';
+//   }
+//   return config;
+// };
 
 // resolve: {
 //     fallback: {
