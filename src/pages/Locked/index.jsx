88b57aaa0576a633586f04fc1a428ac276eaae5d
github_ayns789 <joly.a.idf@gmail.com>
2023-03-24 21:39:15 +0100

    up algo locked page

--- src/pages/Locked/index.jsx
+++ src/pages/Locked/index.jsx
@@ -51,7 +51,7 @@ const Locked = () => {
       const disconnectTime = JSON.parse(localStorage.getItem('disconnectTime'));
       if (disconnectTime) {
         const remainingTime = 1000 * 60 * 60 * 2 - (Date.now() - JSON.parse(disconnectTime));
-        if (remainingTime > 0) {
+        if (remainingTime <= 0) {
           setTimeout(() => {
             dispatch({ type: 'CONNECTED_OFF' });
             localStorage.setItem('user', null);
